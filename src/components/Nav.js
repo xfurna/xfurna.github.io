ff909b0a906d9a7b185d04beac69ef9a05e7ca14
evi1haxor <architdwivedi.off@gmail.com>
2021-12-09 19:46:10 +0530

    navbar implementation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,14 +4,24 @@ import { NavLink } from 'react-router-dom';
 
 export const Nav = () => {
     return (
-        <div className="menu">
-            <div className="navbar">
-                <NavLink className="page" to="/">xf</NavLink>
-                <NavLink className="page" to="/projects">Schism</NavLink>
-                <NavLink className="page" to="/e1ixir">E1ixir</NavLink>
-                <NavLink className="page" to="/about">About</NavLink>
+        <>
+            <div className="menu">
+                <div className="navbar">
+                    <NavLink className="page xf-page" to="/">xf</NavLink>
+                    <NavLink className="page" to="/projects">schism</NavLink>
+                    <NavLink className="page" to="/e1ixir">e1ixir</NavLink>
+                    <NavLink className="page" to="/about">about</NavLink>
+                </div>
             </div>
-        </div>
+            <div className="smenu">
+                <div className="snavbar">
+                    <NavLink className="spage sxf-page" to="/">xf</NavLink>
+                    <NavLink className="spage" to="/projects">Schism</NavLink>
+                    <NavLink className="spage" to="/e1ixir">E1ixir</NavLink>
+                    <NavLink className="spage" to="/about">About</NavLink>
+                </div>
+            </div>
+        </>
     );
 }
 
