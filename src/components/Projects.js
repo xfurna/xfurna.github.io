13e37e7b0cf959c475b9fab0324f8fc730a3a59f
evi1haxor <architdwivedi.off@gmail.com>
2021-12-12 20:04:50 +0530

    make project card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,87 @@ const Projects = () => {
         <>
             <Nav />
             <div className="heading" data-text='work'></div>
+            <div className='project'>
+                <div className='project-head'>RISysssnG</div>
+                <div className='project-outline'>
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />1
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />
+                    this is project outline!!!<br />
+                </div>
+            </div>
+
+            <hr />
+
+            <div className='project'>
+                <div className='project-head'>RISynG</div>
+                <div className='project-outline'>
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                </div>
+            </div>
+
+            <hr />
+
+            <div className='project'>
+                <div className='project-head'>RISynG</div>
+                <div className='project-outline'>
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                </div>
+            </div>
+
+            <hr />
+
+            <div className='project'>
+                <div className='project-head'>RISynG</div>
+                <div className='project-outline'>
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                    this is project outline!!!
+                </div>
+            </div>
             <div className="heading" data-text='बे-तरतीब' style={{ fontFamily: 'Tillana' }}></div>
         </>
     );
