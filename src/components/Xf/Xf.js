56f64ea1b4dfd08de18972d29adddd0949e6e821
evi1haxor <architdwivedi.off@gmail.com>
2021-12-20 20:39:48 +0530

    del

--- src/components/Xf/Xf.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Xf.css';
-const Xf = () => {
-    return (
-        <div align='center' className="xf" data-text="xfurna">xfurna</div>
-    );
-}
-
-export default Xf;
