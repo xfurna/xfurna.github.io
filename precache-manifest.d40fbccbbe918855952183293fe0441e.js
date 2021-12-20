00132e7c36245f58925ede81e35f8260930de73d
evi1haxor <architdwivedi.off@gmail.com>
2021-12-20 20:48:20 +0530

    Updates

---
+++ precache-manifest.d40fbccbbe918855952183293fe0441e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f71a73f5fa91c094af2",
+    "url": "/xf/static/css/main.4c4643cd.chunk.css"
+  },
+  {
+    "revision": "3f71a73f5fa91c094af2",
+    "url": "/xf/static/js/main.3f71a73f.chunk.js"
+  },
+  {
+    "revision": "2eeb5cf8765707a42baf",
+    "url": "/xf/static/js/1.2eeb5cf8.chunk.js"
+  },
+  {
+    "revision": "f55f23560773ae230c9a",
+    "url": "/xf/static/js/2.f55f2356.chunk.js"
+  },
+  {
+    "revision": "c240ce44cb081e1c3993",
+    "url": "/xf/static/js/runtime~main.c240ce44.js"
+  },
+  {
+    "revision": "3d86e6784dc4e837aa17423f0c90e67c",
+    "url": "/xf/static/media/me2.3d86e678.jpeg"
+  },
+  {
+    "revision": "eb59e1b3cd9e884c0887a744850fcaaa",
+    "url": "/xf/index.html"
+  }
+];
