89e37fd4c01e5baa82251805efbb7fc96ff14d0b
evi1haxor <architdwivedi.off@gmail.com>
2021-12-21 15:16:39 +0530

    Updates

---
+++ precache-manifest.4919cfba2d2cada7e88be2653dec7adc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2e149fe81e1cf76db4b",
+    "url": "/static/css/main.99b2bd45.chunk.css"
+  },
+  {
+    "revision": "e2e149fe81e1cf76db4b",
+    "url": "/static/js/main.e2e149fe.chunk.js"
+  },
+  {
+    "revision": "2eeb5cf8765707a42baf",
+    "url": "/static/js/1.2eeb5cf8.chunk.js"
+  },
+  {
+    "revision": "f55f23560773ae230c9a",
+    "url": "/static/js/2.f55f2356.chunk.js"
+  },
+  {
+    "revision": "90be61811ef5a0f46f76",
+    "url": "/static/js/runtime~main.90be6181.js"
+  },
+  {
+    "revision": "3d86e6784dc4e837aa17423f0c90e67c",
+    "url": "/static/media/me2.3d86e678.jpeg"
+  },
+  {
+    "revision": "95f44133127182f0928d5c8353975efd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/xf/precache-manifest.daf27db89b8af904c3bff13efb6ecb40.js"
+  "/precache-manifest.4919cfba2d2cada7e88be2653dec7adc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/xf/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
