eca07e17f77989ff8b5962816a21d35740a131cb
xfurna <architdwivedi.off@gmail.com>
2023-05-07 01:19:59 +0530

    Updates

---
+++ precache-manifest.2c691143ca3344b2f9ac6eec5aa2fddd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "867b446c50f4d9f0869f",
+    "url": "/static/css/main.4473b053.chunk.css"
+  },
+  {
+    "revision": "867b446c50f4d9f0869f",
+    "url": "/static/js/main.867b446c.chunk.js"
+  },
+  {
+    "revision": "885e3e8dbf7f9941daa7",
+    "url": "/static/js/1.885e3e8d.chunk.js"
+  },
+  {
+    "revision": "4a539e4bf16a07685756",
+    "url": "/static/js/2.4a539e4b.chunk.js"
+  },
+  {
+    "revision": "d9ca3f30e89f0b3afb30",
+    "url": "/static/js/runtime~main.d9ca3f30.js"
+  },
+  {
+    "revision": "104377f8718596db184c08bd147b5622",
+    "url": "/static/media/me2.104377f8.jpg"
+  },
+  {
+    "revision": "b80e6a32ed1153fe9e6131f6e20cadfb",
+    "url": "/index.html"
+  }
+];
